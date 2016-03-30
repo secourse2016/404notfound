2f28b661e6a84f328d8939205cbcf7dd43e0ad88
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-03-30 11:08:26 +0200

    Integrate Server

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var util = require('util');
+var app = express();
+
+
+app.use('/', express.static('public'));
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
