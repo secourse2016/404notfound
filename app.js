cf5a2cdc3c569872ee10bf71086dda725a6a03a4
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-03-30 23:12:52 +0200

    Change port number

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var util = require('util');
 var app = express();
 
 
-app.use('', express.static('public'));
+app.use('/', express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function() {
+  console.log('Example app listening on port 8080!');
 });
