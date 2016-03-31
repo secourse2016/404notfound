9b0f3aef94ade23313436a0a997d435f00bb5edb
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-03-31 12:39:14 +0200

    Create binding example between passenger view and passenger controller

--- public/js/controllers/passengerDetailsController.js
+++ public/js/controllers/passengerDetailsController.js
@@ -3,9 +3,13 @@ App.controller('passengerDetailsCtrl',function($scope,$location) {
     $scope.title = "Fill in your details";
     $scope.buttonText = "Next";
     $scope.goNext = function(){
+      ///before you leave the page make sure that the passenger object is complete otherwise show alert("Fill in all data");
+      console.log($scope.passenger);
         $location.path('/seating');
     }
     //yasmine you're not expecting any parameters. You're only creating a form and you will have
     // the resulting object ready to be sent to the server
 
+
+    $scope.passenger = {};
 });
