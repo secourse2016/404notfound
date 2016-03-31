ff26c0583855216583f82f473d402df02baded05
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2016-03-31 13:27:45 +0200

    Adding the back buttons

--- public/js/controllers/confirmationController.js
+++ public/js/controllers/confirmationController.js
@@ -1,9 +1,14 @@
 // @abdelrhman-essam
 App.controller('confirmationCtrl',function($scope,$location){
   $scope.title = "Confirm your flight";
-  $scope.buttonText = "Confirm?";
+
+  $scope.buttonTextNxt = "Confirm?";
+  $scope.buttonTextBk = "Back";
   $scope.goNext = function(){
       $location.path('/');
+  }
+  $scope.goBack = function(){
+      $location.path('/payment');
   }
 
   //You shouldn't add much here for now
--- public/js/controllers/passengerDetailsController.js
+++ public/js/controllers/passengerDetailsController.js
@@ -1,9 +1,14 @@
 // @yassmine
 App.controller('passengerDetailsCtrl',function($scope,$location) {
     $scope.title = "Fill in your details";
-    $scope.buttonText = "Next";
+
+    $scope.buttonTextNxt = "Next";
+    $scope.buttonTextBk = "Back";
     $scope.goNext = function(){
         $location.path('/seating');
+    }
+    $scope.goBack = function(){
+        $location.path('/exit-flight');
     }
     //yasmine you're not expecting any parameters. You're only creating a form and you will have
     // the resulting object ready to be sent to the server
