4a2036e10ba59f68682665fb6eea8d0623cf83a1
ar-maged <abdulrahman.maged95@gmail.com>
2016-03-31 14:20:17 +0200

    Fix collapse

--- public/js/controllers/flightsController.js
+++ public/js/controllers/flightsController.js
@@ -1,11 +1,16 @@
 // @abdelrahman-maged
-App.controller('flightsCtrl',function($scope,$location){
+App.controller('flightsCtrl', function($scope, $location) {
+
   $scope.title = "Choose a Flight";
   $scope.buttonText = "Next";
-  $scope.goNext = function(){
+  $scope.isCollapsed = true;
+
+  $scope.goNext = function() {
       $location.path('/exit-flight');
-  }
+    }
+
   // Abdo expect that you have the origin, destination and flight time
   // var flightsArray = ourSuperCrazyFactory.get(org,des,time);
   //create fake array and bind it to your view
+  
 });
