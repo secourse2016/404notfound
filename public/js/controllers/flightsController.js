a1092918eb4764b760fec7d794392bfb0c8fe21d
ar-maged <abdulrahman.maged95@gmail.com>
2016-03-31 16:11:05 +0200

    Data binding

--- public/js/controllers/flightsController.js
+++ public/js/controllers/flightsController.js
@@ -10,9 +10,33 @@ App.controller('flightsCtrl',function($scope,$location){
   $scope.goBack = function(){
       $location.path('/');
   }
-  //if you have any other assumtions tell me.
 
-  // Abdo expect that you have the origin, destination and flight time
-  // var flightsArray = ourSuperCrazyFactory.get(org,des,time);
-  //create fake array and bind it to your view
+  $scope.flights = [
+    {
+      "number": "1000",
+      "departureUTC": "2016-04-01T07:00:00Z",
+      "arrivalUTC": "2016-04-01T09:00:00Z",
+      "duration": 2.0,
+      "status": "On Time",
+      "refAircraftTailNumber": "D-AAAA",
+      "refAircraftModel": "Airbus A330", // nonexistent in flight schema, needed however in view
+      "operatorAirline": "Air Berlin",
+      "refOriginAirport": "CAI",
+      "refOriginAirportName": "Cairo International Airport", // nonexistent in flight schema, needed however in view
+      "refDestinationAirport": "TXL",
+      "refDestinationAirportName": "Berlin-Tegel Airport", // nonexistent in flight schema, needed however in view
+      "boardingGate": "40",
+      "boardingPeriod": 45.0,
+      "boardingTerminal": "3",
+      "arrivalTerminal": "1",
+      "economyFare": 200.0,
+      "businessFare": 300.0,
+      "emptyEconomySeatsCount": 320, // retrieved in backend from aircrafts collection
+      "emptyBusinessSeatsCount": 24, // retrieved in backend from aircrafts collection
+      "economySeatSchema": null,
+      "buisnessSeatSchema": null,
+      "seatmap": null
+    }
+  ];
+
 });
