9f6401b632815390d54fb694883101f7f9dfd70e
ar-maged <abdulrahman.maged95@gmail.com>
2016-03-31 20:32:49 +0200

    Populating search results

--- public/js/controllers/flightsController.js
+++ public/js/controllers/flightsController.js
@@ -32,7 +32,32 @@ App.controller('flightsCtrl',function($scope,$location){
       "arrivalTerminal": "1",
       "economyFare": 200.0,
       "businessFare": 300.0,
-      "emptyEconomySeatsCount": 320, // retrieved in backend from aircrafts collection
+      "emptyEconomySeatsCount": 14, // retrieved in backend from aircrafts collection
+      "emptyBusinessSeatsCount": 20, // retrieved in backend from aircrafts collection
+      "economySeatSchema": null,
+      "buisnessSeatSchema": null,
+      "seatmap": null
+    },
+    {
+      "number": "1001",
+      "departureUTC": "2016-04-01T15:00:00Z",
+      "arrivalUTC": "2016-04-01T17:00:00Z",
+      "duration": 120,
+      "status": "On Time",
+      "refAircraftTailNumber": "D-AAAA",
+      "refAircraftModel": "Airbus A330", // nonexistent in flight schema, needed however in view
+      "operatorAirline": "Air Berlin",
+      "refOriginAirport": "CAI",
+      "refOriginAirportName": "Cairo International Airport", // nonexistent in flight schema, needed however in view
+      "refDestinationAirport": "TXL",
+      "refDestinationAirportName": "Berlin-Tegel Airport", // nonexistent in flight schema, needed however in view
+      "boardingGate": "40",
+      "boardingPeriod": 45.0,
+      "boardingTerminal": "3",
+      "arrivalTerminal": "1",
+      "economyFare": 200.0,
+      "businessFare": 300.0,
+      "emptyEconomySeatsCount": 50, // retrieved in backend from aircrafts collection
       "emptyBusinessSeatsCount": 24, // retrieved in backend from aircrafts collection
       "economySeatSchema": null,
       "buisnessSeatSchema": null,
