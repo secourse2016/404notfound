a68b1980851fcde3ce4129b7467b10393c3522b6
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-04-01 14:10:52 +0200

    Initial Angular binding with main view

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -11,4 +11,36 @@ App.controller('mainCtrl',function($scope,$location){
   $scope.goToFlights = function(){
     $location.path('/flights');
   };
+
+setUpDate($scope);
+
 });
+
+function setUpDate($scope){
+  $scope.today = function() {
+  $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.open2 = function() {
+   $scope.popup2.opened = true;
+  };
+  $scope.setDate = function(year, month, day) {
+     $scope.dt = new Date(year, month, day);
+   };
+   function disabled(data) {
+     var date = data.date,
+       mode = data.mode;
+     return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+   }
+   $scope.dateOptions = {
+     dateDisabled: disabled,
+     formatYear: 'yy',
+     maxDate: new Date(2020, 5, 22),
+     minDate: new Date(),
+     startingDay: 1
+   };
+   $scope.popup2 = {
+     opened: false
+   };
+}
