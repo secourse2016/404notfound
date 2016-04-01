76ae34aa5de68211130f1cebb1edd61e76a4f1ed
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-04-02 00:33:45 +0200

    Modify payment view

--- public/js/controllers/paymentController.js
+++ public/js/controllers/paymentController.js
@@ -11,6 +11,16 @@ App.controller('paymentCtrl', function($scope, $location) {
     $location.path('/seating');
   }
 
+  $scope.years = ['2016','2017','2018','2019','2020','2021','2022','2023','2024'];
+  $scope.yearsBtnText = $scope.years[0];
+  $scope.changeYear = function(text) {
+    $scope.yearsBtnText = text;
+  }
 
+  $scope.months = ['January','Feburary','March','April','May','June','July','August','September','October','November','December'];
+  $scope.monthsBtnText = $scope.months[0];
+  $scope.changeMonth = function(text) {
+    $scope.monthsBtnText = text;
+  }
   //mirna nty gaya thazary l sprint deh bs khoshy ma3a Ahmed fel seatings bta3to
 });
