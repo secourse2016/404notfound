e679678a5d98755f9ae0a317c6068d592ad987e2
abdelrhman essam <abdelrhman.essam1995@gmail.com>
2016-04-01 23:36:58 +0200

    updated confirmation view

--- public/js/controllers/confirmationController.js
+++ public/js/controllers/confirmationController.js
@@ -1,15 +1,22 @@
 // @abdelrhman-essam
-App.controller('confirmationCtrl',function($scope,$location){
+App.controller('confirmationCtrl', function($scope, $location) {
   $scope.title = "Confirm your flight";
 
   $scope.buttonTextNxt = "Confirm?";
   $scope.buttonTextBk = "Back";
-  $scope.goNext = function(){
-      $location.path('/');
+  $scope.goNext = function() {
+    $location.path('/');
   }
-  $scope.goBack = function(){
-      $location.path('/payment');
+  $scope.goBack = function() {
+    $location.path('/payment');
   }
 
   //You shouldn't add much here for now
+  //you might expect passenger and booking data
+
+  // this is just a test
+  $scope.booking = {
+    wifi: "true",
+    name: "ahmed"
+  }
 });
