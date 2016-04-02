d2f3ece34caa2b9f58f40da8a7f817adc13aa94f
ar-maged <abdulrahman.maged95@gmail.com>
2016-04-02 20:48:09 +0200

    Replace homepage catchlines

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,7 +1,7 @@
 App.controller('mainCtrl', function($scope, $location, api) {
   $('#main-text').typeIt({
     strings: [
-      "We as a company care about nothing but money....", "But you will enjoy it....", "Bgd bgd hamada...", "Msh mesada2? ta3ala garab....."
+      "Simple, convenient, instant confirmation.", "Destinations all around the globe.", "Experience authentic hospitality.", "Time to get enchanted."
     ],
     speed: 120,
     breakLines: false,
