efdeed48e36814a35ebb92795f4c4a94594b7d5e
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2016-04-03 11:50:49 +0200

    Flight using flight & booking from factory functions

--- public/js/controllers/flightController.js
+++ public/js/controllers/flightController.js
@@ -10,57 +10,52 @@ App.controller('exitFlightCtrl',function($scope,$location,api){
   $scope.goBack = function(){
       $location.path('/flights');
   }
-// ​
-//   // if(!api.getChosenFlight()){
-//   //   $location.path('/flights');
-//   // }
-// ​
-var flight=  {
-    "number": "1000",
-    "departureUTC": "2016-04-01T07:00:00Z",
-    "arrivalUTC": "2016-04-01T09:00:00Z",
-    "duration": 120.0,
-    "status": "On Time",
-    "refAircraftTailNumber": "D-AAAA",
-    "operatorAirline": "Air Berlin",
-    "refOriginAirport": "CAI",
-    "refOriginAirportName": "Cairo International Airport", // nonexistent in flight schema, needed however in view
-    "refDestinationAirport": "TXL",
-    "refDestinationAirportName": "Berlin-Tegel Airport",  // nonexistent in flight schema, needed however in view
-    "boardingGate": "40",
-    "boardingPeriod": 45.0,
-    "boardingTerminal": "3",
-    "arrivalTerminal": "1",
-    "economyFare": 200.0,
-    "businessFare": 300.0,
-    "emptyEconomySeatsCount": null,
-    "emptyBusinessSeatsCount": null,
-    "economySeatSchema": null,
-    "buisnessSeatSchema": null,
-    "seatmap": null
-  };
-  //api.getChosenFlight();
-
-
-  var booking = {
-  "_id": "A235647925",
-  "refPassengerID": ["C673482992G"],
-  "exitDepartureUTC": "2016-04-01T07:00:00Z",
-  "reEntryDepartureUTC": null,
-  "issueDate": "2016-03-30T06:00:00Z",
-  "isEconomy": true,
-  "isOneWay": true,
-  "refExitFlightNumber": "1000",
-  "refReEntryFlightNumber": null,
-  "receiptNumber": "054"
-};
-//api.getBooking();
-
+  if(!api.getChosenFlight() || !api.getBooking()){
+    $location.path('/flights');
+  }
+var flight= api.getChosenFlight();
+// {
+//     "number": "1000",
+//     "departureUTC": "2016-04-01T07:00:00Z",
+//     "arrivalUTC": "2016-04-01T09:00:00Z",
+//     "duration": 120.0,
+//     "status": "On Time",
+//     "refAircraftTailNumber": "D-AAAA",
+//     "operatorAirline": "Air Berlin",
+//     "refOriginAirport": "CAI",
+//     "refOriginAirportName": "Cairo International Airport", // nonexistent in flight schema, needed however in view
+//     "refDestinationAirport": "TXL",
+//     "refDestinationAirportName": "Berlin-Tegel Airport",  // nonexistent in flight schema, needed however in view
+//     "boardingGate": "40",
+//     "boardingPeriod": 45.0,
+//     "boardingTerminal": "3",
+//     "arrivalTerminal": "1",
+//     "economyFare": 200.0,
+//     "businessFare": 300.0,
+//     "emptyEconomySeatsCount": null,
+//     "emptyBusinessSeatsCount": null,
+//     "economySeatSchema": null,
+//     "buisnessSeatSchema": null,
+//     "seatmap": null
+//   };
+  var booking = api.getBooking();
+//   {
+//   "_id": "A235647925",
+//   "refPassengerID": ["C673482992G"],
+//   "exitDepartureUTC": "2016-04-01T07:00:00Z",
+//   "reEntryDepartureUTC": null,
+//   "issueDate": "2016-03-30T06:00:00Z",
+//   "isEconomy": true,
+//   "isOneWay": true,
+//   "refExitFlightNumber": "1000",
+//   "refReEntryFlightNumber": null,
+//   "receiptNumber": "054"
+// };
 var facilities = ["Smoking areas available", "Wi-Fi availability",
 "4 cultural cuisines", "Inflight entertainment", "Extra cozy sleeperette",
  "Screens to show your flight pattern, aircraft altitude and speed"];
 
- var departureDate = new Date(flight.departureUTC);
+var departureDate = new Date(flight.departureUTC);
      flight.departureUTC = departureDate.toUTCString();
 var arrivalDate = new Date(flight.arrivalUTC);
      flight.arrivalUTC = arrivalDate.toUTCString();
