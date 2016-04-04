3ac8d7d7b5b6dc4a8ab9cbcf4abe896d012ea575
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-04-04 19:02:33 +0200

    added alerts (but they are commented)

--- public/js/controllers/passengerDetailsController.js
+++ public/js/controllers/passengerDetailsController.js
@@ -51,7 +51,7 @@ App.controller('passengerDetailsCtrl', function($scope, $location, api) {
   $location.path('/flights');
   alert("You have to choose a flight");
 }*/
-//The reverting to the flights page
+//This reverting always happens because the chosen flight is always null so the passenger details view doesnt appear
 
     $scope.passenger = {
       _id: "A938020", //not sure how it is generated
@@ -86,6 +86,7 @@ App.controller('passengerDetailsCtrl', function($scope, $location, api) {
         if(($scope.firstName ==null)||($scope.middleName ==null)||($scope.lastName ==null)||($scope.phoneNumber ==null)||($scope.passportNumber ==null))
         {
           alert("Please fill in data");
+
           console.log(complete);
 
         }
