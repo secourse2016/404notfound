3384bc19ffceb4073b10db2c86fc9e55cd0f6f94
A.R. Maged <abdulrahman.maged95@gmail.com>
2016-04-15 09:08:01 +0200

    Accommodate aircrafts-airports routes with callbacks
    Accommodate aircrafts-airports routes to receive callbacks from DB instead of returns

--- routes/aircrafts-airports.js
+++ routes/aircrafts-airports.js
@@ -6,19 +6,25 @@ var airports = require('../mockdata/airports');
 
 // this route should return all the airports from json file
 router.get('/airports', function(req, res) {
-  res.send(db.getAirports());
+  db.getAirports(function(err, airports){
+    res.send(airports);
+  });
 })
 
 // this should return all the aircrafts
 router.get('/aircrafts', function(req, res) {
-  res.send(db.getAircrafts());
+  db.getAircrafts(function(err, aircrafts){
+    res.send(aircrafts);
+  });
 })
 
 // this should return one aircraft only specified in the id
 router.get('/aircrafts/:id', function(req, res) {
     // this the id that will be sent to you from the client
     var flightId = req.params.id;
-    res.send(db.getAircraft(flightId));
+    db.getAircraft(flightId, function(err, aircraft){
+      res.send(aircraft);
+    });
 })
 
 module.exports = router;
