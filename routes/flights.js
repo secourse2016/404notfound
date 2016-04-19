7deef14b05e717aedc7f2f0804400b84b0440a37
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-04-19 10:06:47 +0200

    Accommodate return flights

--- routes/flights.js
+++ routes/flights.js
@@ -159,15 +159,139 @@ router.get('/flights/search/:origin/:destination/:departingDate/:returningDate/:
 
     var flightClass = req.params.class;
 
+    var originalRes = res;
+    var result = {
+        outgoingFlights: [],
+        returnFlights: []
+    };
     db.getFlights(origin, destination, departingDate, returningDate, false, function(err, flights) {
         if (err)
             console.log(err);
 
-        if (req.headers['website'] == 'AirBerlin') {
-            res.send(flights);
-        } else {
+            if (req.headers['website'] == 'AirBerlin' && req.headers['otherHosts'] == 'false') {
+                originalRes.send(flights);
+                return;
+            } else if( req.headers['other-hosts'] == 'true'){
+                function httpGet(url, callback) {
+                    const options = {
+                        uri: url + '/api/flights/search/' + origin + "/" + destination + "/" + req.params.departingDate+"/"+req.params.returningDate + "/" + flightClass,
 
-        }
+                        headers: {
+                            'x-access-token': accessToken,
+                        },
+                        json: true
+                    };
+                    request(options,
+                        function(err, res, body) {
+                          // console.log(err + options.uri)
+
+                            callback(err, res);
+                        }
+
+                    );
+                }
+
+                async.map(endpointsUrls, httpGet, function(err, res) {
+                    if (err) {
+                        originalRes.send(err)
+                        return console.log(err);
+                    }
+                    parseResutl(res);
+
+                })
+
+                function parseResutl(res) {
+                    for (var i = 0; i < res.length; i++) {
+                        if (res[i].body.outgoingFlights)
+                            result.outgoingFlights.push(res[i].body.outgoingFlights)
+                        if (res[i].body.returnFlights)
+                            result.returnFlights.push(res[i].body.returnFlights)
+                    }
+                    result.outgoingFlights = [].concat.apply([], result.outgoingFlights);
+                    result.returnFlights = [].concat.apply([], result.returnFlights);
+
+                    originalRes.send(result)
+                    return;
+                }
+
+                // async.parallel(result,function(){
+                //     res.send(result);
+                //   })
+            }else{
+              var flightsModified = {
+                outgoingFlights:[],
+                returnFlights:[]
+              }
+              for (var i = 0; i < flights.outgoingFlights.length; i++) {
+                if(flightClass == 'economy')
+                var flightModified = {
+                  "flightNumber"      : flights.outgoingFlights[i].number,
+                  "aircraftType"      : flights.outgoingFlights[i].refAircraftTailNumber,
+                  "aircraftModel"     :  flights.outgoingFlights[i].refAircraftModel,
+                  "departureDateTime" : (new Date(flights.outgoingFlights[i].departureUTC)).getTime(),
+                  "arrivalDateTime"   : (new Date(flights.outgoingFlights[i].arrivalUTC)).getTime(),
+                  "origin"            : flights.outgoingFlights[i].refOriginAirport,
+                  "destination"       : flights.outgoingFlights[i].refDestinationAirport,
+                  "cost"              : flights.outgoingFlights[i].economyFare,
+                  "currency"          : "USD",
+                  "class"             : "Economy",
+                  "Airline"           : "Air Berlin"
+                }
+                else
+                var flightModified = {
+                  "flightNumber"      : flights.outgoingFlights[i].number,
+                  "aircraftType"      : flights.outgoingFlights[i].refAircraftTailNumber,
+                  "aircraftModel"     :  flights.outgoingFlights[i].refAircraftModel,
+                  "departureDateTime" : (new Date(flights.outgoingFlights[i].departureUTC)).getTime(),
+                  "arrivalDateTime"   : (new Date(flights.outgoingFlights[i].arrivalUTC)).getTime(),
+                  "origin"            : flights.outgoingFlights[i].refOriginAirport,
+                  "destination"       : flights.outgoingFlights[i].refDestinationAirport,
+                  "cost"              : flights.outgoingFlights[i].businessFare,
+                  "currency"          : "USD",
+                  "class"             : "Business",
+                  "Airline"           : "Air Berlin"
+                }
+                flightsModified.outgoingFlights.push(flightModified);
+              }
+
+
+              for (var i = 0; i < flights.returnFlights.length; i++) {
+                if(flightClass == 'economy')
+                var flightModified = {
+                  "flightNumber"      : flights.returnFlights[i].number,
+                  "aircraftType"      : flights.returnFlights[i].refAircraftTailNumber,
+                  "aircraftModel"     :  flights.returnFlights[i].refAircraftModel,
+                  "departureDateTime" : (new Date(flights.returnFlights[i].departureUTC)).getTime(),
+                  "arrivalDateTime"   : (new Date(flights.returnFlights[i].arrivalUTC)).getTime(),
+                  "origin"            : flights.returnFlights[i].refOriginAirport,
+                  "destination"       : flights.returnFlights[i].refDestinationAirport,
+                  "cost"              : flights.returnFlights[i].economyFare,
+                  "currency"          : "USD",
+                  "class"             : "Economy",
+                  "Airline"           : "Air Berlin"
+                }
+                else
+                var flightModified = {
+                  "flightNumber"      : flights.returnFlights[i].number,
+                  "aircraftType"      : flights.returnFlights[i].refAircraftTailNumber,
+                  "aircraftModel"     :  flights.returnFlights[i].refAircraftModel,
+                  "departureDateTime" : (new Date(flights.returnFlights[i].departureUTC)).getTime(),
+                  "arrivalDateTime"   : (new Date(flights.returnFlights[i].arrivalUTC)).getTime(),
+                  "origin"            : flights.returnFlights[i].refOriginAirport,
+                  "destination"       : flights.returnFlights[i].refDestinationAirport,
+                  "cost"              : flights.returnFlights[i].businessFare,
+                  "currency"          : "USD",
+                  "class"             : "Business",
+                  "Airline"           : "Air Berlin"
+                }
+                flightsModified.returnFlights.push(flightModified);
+              }
+              console.log(flightsModified)
+              originalRes.send(flightsModified)
+              return;
+            }
+
+
 
     });
 });
