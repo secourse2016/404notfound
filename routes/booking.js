d0c94184fe7471a10109af36327e230920efb41a
ar-maged <abdulrahman.maged95@gmail.com>
2016-04-29 17:51:11 +0200

    Format code booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -3,10 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 
-//this function is the core of our app, it inserts the passenger data into the database,
-// update the selected seat and insert the booking object into the database
+/*this function is the core of our app, it inserts the passenger data into the database,
+  updates the selected seat and inserts the booking object into the database*/
+
 router.post("/booking", function(req, res) {
+
   if (req.headers['other-hosts'] == 'false') {
+
     var Passenger = req.body.passenger;
     var booking = req.body.booking;
     var exitFlightID = booking.refExitFlightID;
@@ -15,58 +18,83 @@ router.post("/booking", function(req, res) {
     var passengersIds = [];
     var exitIsEconomy, reEntryIsEconomy;
     var outgoingSeatNumber, returnSeatNumber;
-    // console.log(booking)
 
     db.postPassengers(Passenger, function(err, data) {
+
       if (!err) {
 
         passengersIds.push(data.ops[0]._id);
         booking.refPassengerID.push(passengersIds[0]);
+
         db.postBooking(booking, function(err, data) {
+
           if (!err) {
+
             bookingId = data.ops[0]._id;
             exitIsEconomy = booking.exitIsEconomy;
             outgoingSeatNumber = req.body.outgoingSeatNumber;
 
             res.send("Your booking was submitted succefully");
+
             db.updateFlight(false, exitFlightID, exitIsEconomy, outgoingSeatNumber, passengersIds, bookingId, function(err, data) {
+
               if (!err) {
+
                 if (returnFlightID) {
+
                   reEntryIsEconomy = booking.reEntryIsEconomy;
                   returnSeatNumber = req.body.returnSeatNumber;
 
                   db.updateFlight(false, returnFlightID, reEntryIsEconomy, returnSeatNumber, passengersIds, bookingId, function(err, data) {
+
                     if (!err) {
+
                       res.send({
                         refNum: booking.receiptNumber,
                         errorMessage: err
                       });
+
                       console.log('successfully added your booking');
                       return;
+
                     } else {
+
                       res.send({
                         refNum: booking.receiptNumber,
                         errorMessage: err
                       });
+
                       console.log('error occured while adding your booking');
                       return;
+
                     }
+
                   });
+
                 }
+
               } else {
+
                 res.send({
                   refNum: booking.receiptNumber,
                   errorMessage: err
                 });
+
                 console.log('error occured while adding your booking');
                 return;
+
               }
+
             });
+
           }
+
         });
+
       }
 
     });
+
   } else {
 
     var Passengers = req.body.booking.passengerDetails;
@@ -81,10 +109,6 @@ router.post("/booking", function(req, res) {
 
   }
 
-
-
-
-
-})
+});
 
 module.exports = router;
