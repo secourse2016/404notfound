1054d242183e4c5adeee89d0810a71b22464ca3a
abdelrhman essam <abdelrhman.essam1995@gmail.com>
2016-04-30 18:00:13 +0200

    edited passenger details mobile view (alerts)

--- public/shared/js/controllers/passengerDetailsController.js
+++ public/shared/js/controllers/passengerDetailsController.js
@@ -1,35 +1,67 @@
 // @yassmine
-App.controller('passengerDetailsCtrl', function($scope, $location, api, $ionicPopup) {
+App.controller('passengerDetailsCtrl', function($scope, $location, api) {
   $scope.title = "Fill in your details";
 
   $scope.buttonTextNxt = "Next";
   $scope.buttonTextBk = "Back";
 
-if(Type == 'desktop'){
-  if(!api.getChosenOutGoingFlight() || !api.getBooking()){
-    $location.path('/flights');
-    return;
-  }
-
-  $scope.titles = ['Mr', 'Mrs', 'Ms', 'Dr'];
-  $scope.titlesBtnText = $scope.titles[0];
-  $scope.changeTitle = function(text) {
-    $scope.titlesBtnText = text;
-  }
-
-  api.getCountries().then(function mySucces(response) {
-    $scope.countries = response.data;
-  }, function myError(response) {
-    console.log(response.statusText);
-  });
-
-
-
-
   $scope.passenger = {
+    type: null,
+    countryCode: null,
+    nationality: null,
+    sex: null,
+    birthDate: null,
+    birthPlace: null,
+    nationalID: null,
+    authority: null,
+    issueDate: null,
+    expiryDate: null,
+    points: null,
+    membership: null,
+    firstName: null,
+    middleName: null,
+    lastName: null,
+    passportNumber: null,
+    phoneNumber: null,
+    email: null
+
+  };
+  if (Type == 'desktop') {
+    if (!api.getChosenOutGoingFlight() || !api.getBooking()) {
+      $location.path('/flights');
+      return;
+    }
+
+    $scope.titles = ['Mr', 'Mrs', 'Ms', 'Dr'];
+    $scope.titlesBtnText = $scope.titles[0];
+    $scope.changeTitle = function(text) {
+      $scope.titlesBtnText = text;
+    }
+
+    api.getCountries().then(function mySucces(response) {
+      $scope.countries = response.data;
+    }, function myError(response) {
+      console.log(response.statusText);
+    });
+
+
+
+
+    // ---------------------------------------- Now you have $scope.nationality and $scope.titlesBtnText you can use them in your object
+    var complete = false;
+    $scope.goNext = function() {
+
+      /*if(!api.getChosenFlight())
+      {
+      $location.path('/flights');
+      alert("You have to choose a flight");
+      }*/
+      //The reverting to the flights page
+
+      $scope.passenger = {
         type: null,
-        countryCode: null,
-        nationality:null,
+        countryCode: null, //according to country
+        nationality: $scope.nationality,
         sex: null,
         birthDate: null,
         birthPlace: null,
@@ -39,29 +71,61 @@ if(Type == 'desktop'){
         expiryDate: null,
         points: null,
         membership: null,
-       firstName :null,
-        middleName: null,
-        lastName:null,
-        passportNumber: null,
-        phoneNumber:null,
-        email: null
+        title: $scope.titlesBtnText,
+        firstName: $scope.firstName,
+        middleName: $scope.middleName,
+        lastName: $scope.lastName,
+        passportNumber: $scope.passportNumber,
+        phoneNumber: $scope.phoneNumber,
+        email: $scope.email1
 
-       };
-  // ---------------------------------------- Now you have $scope.nationality and $scope.titlesBtnText you can use them in your object
-  var complete = false;
-  $scope.goNext = function() {
 
-  /*if(!api.getChosenFlight())
-  {
-  $location.path('/flights');
-  alert("You have to choose a flight");
-  }*/
-  //The reverting to the flights page
+      };
+      ///before you leave the page make sure that the passenger object is complete otherwise show alert("Fill in all data");
+
+
+
+      if (complete == false) {
+        $scope.alertData = false;
+        if (($scope.firstName == null) || ($scope.middleName == null) || ($scope.lastName == null) || ($scope.phoneNumber == null) || ($scope.passportNumber == null)) {
+          $scope.alertData = true;
+
+        } else {
+          $scope.alertConfirm = false;
+          if ($scope.email1 != $scope.emailver)
+            $scope.alertConfirm = true;
+          else {
+            $scope.alertCheck = false;
+            if (($scope.check == null))
+              $scope.alertCheck = true;
+            else {
+              complete = true;
+            }
+          }
+
+        }
+
+
+      }
+      if (complete == true) {
+        api.setPassenger($scope.passenger);
+        $location.path('/seating/outgoing');
+      }
+
+    }
+    $scope.goBack = function() {
+      $location.path('/exit-flight');
+    }
+  }
+
+
+  $scope.hello = function(){
+
 
     $scope.passenger = {
       type: null,
       countryCode: null, //according to country
-      nationality:$scope.nationality,
+      nationality: $scope.countries,
       sex: null,
       birthDate: null,
       birthPlace: null,
@@ -71,8 +135,8 @@ if(Type == 'desktop'){
       expiryDate: null,
       points: null,
       membership: null,
-      title: $scope.titlesBtnText,
-      firstName : $scope.firstName,
+      title: $scope.Title,
+      firstName: $scope.firstName,
       middleName: $scope.middleName,
       lastName: $scope.lastName,
       passportNumber: $scope.passportNumber,
@@ -80,59 +144,40 @@ if(Type == 'desktop'){
       email: $scope.email1
 
 
-       };
+    };
     ///before you leave the page make sure that the passenger object is complete otherwise show alert("Fill in all data");
 
 
 
-  if(complete == false){
-    $scope.alertData=false;
-        if(($scope.firstName ==null)||($scope.middleName ==null)||($scope.lastName ==null)||($scope.phoneNumber ==null)||($scope.passportNumber ==null))
-        {
-          $scope.alertData = true;
+    if (complete == false) {
 
-        }
-        else{
-          $scope.alertConfirm=false;
-          if($scope.email1!=$scope.emailver)
-      $scope.alertConfirm=true;
+      if (($scope.firstName == null) || ($scope.middleName == null) || ($scope.lastName == null) || ($scope.phoneNumber == null) || ($scope.passportNumber == null)) {
+      alert("Please fill in data");
+
+      } else {
+
+        if ($scope.email1 != $scope.emailver)
+        alert("The entered emails do not match");
+        else {
+
+          if (($scope.check == null))
+          alert("Please verify the information you entered")
           else {
-              $scope.alertCheck=false;
-            if(($scope.check==null))
-          $scope.alertCheck=true;
-            else{
             complete = true;
           }
-          }
-
-        }
-
-
-    }
-      if(complete==true){
-        api.setPassenger($scope.passenger);
-          $location.path('/seating/outgoing');
         }
 
       }
-  $scope.goBack = function() {
-      $location.path('/exit-flight');
+
+
     }
-}
+    if (complete == true) {
+      api.setPassenger($scope.passenger);
+      $location.path('#/tab/seating/outgoing');
+    }
 
+  };
 
-
-
-/*$scope.goAlert = function() {
- $scope.showPopup = function() {
-  var alertPopup = $ionicPopup.alert({
-  title: 'Dont eat that!',
-  template: 'It might taste good'
-});
-alertPopup.then(function(res) {
-     console.log('Thank you for not eating my delicious ice cream cone');
-   });
- };*/
 
 
 });
