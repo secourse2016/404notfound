07ef4494926e3f76986c563494ff48e49471cbef
A.R. Maged <abdulrahman.maged95@gmail.com>
2016-05-01 17:24:46 +0200

    Revert to old flightsNewController.js

--- public/shared/js/controllers/flightsNewController.js
+++ public/shared/js/controllers/flightsNewController.js
@@ -1,65 +1,63 @@
+var flightNewController = function($scope, $location, $routeParams, api) {
 
-var flightNewController = function($scope, $location,$routeParams,api) {
-    $scope.pageClass = 'page-flights';
-    $scope.title = "Choose a Flight";
-    $scope.buttonTextNxt = "Next";
-    $scope.buttonTextBk = "Back";
-    $scope.isCollapsed = false;
-    $scope.isOutgoingFlightSelected = false;
-    api.setIsOtherHosts(true);
+  $scope.pageClass = 'page-flights';
+  $scope.title = "Choose a Flight";
+  $scope.buttonTextNxt = "Next";
+  $scope.buttonTextBk = "Back";
+  $scope.isCollapsed = false;
+  $scope.isOutgoingFlightSelected = false;
 
-    $scope.goNext = function() {
+  api.setIsOtherHosts(true);
 
-        api.setOutGoingFlight($scope.selectedOutgoingFlight);
-        api.setReturningFlight($scope.selectedReturningFlight);
-        api.setBooking($scope.selectedBooking);
+  $scope.goNext = function() {
+    api.setOutGoingFlight($scope.selectedOutgoingFlight);
+    api.setReturningFlight($scope.selectedReturningFlight);
+    api.setBooking($scope.selectedBooking);
+    $location.path('/exit-flight');
+  }
 
+  $scope.goBack = function() {
+    $location.path('/');
+  }
 
+  $scope.selectedBooking = {
+    "PassengerDetails": null,
+    "refExitFlightID": null,
+    "refReEntryFlightID": null,
+    "class": null,
+    "paymentToken": null
+  };
 
-        $location.path('/exit-flight');
+  var origin = $routeParams.origin;
+  var destination = $routeParams.destination;
+  var exitDate = new Date($routeParams.exitDate * 1000);
 
-    }
+  $scope.roundTrip = false;
 
-    $scope.goBack = function() {
-        // check if the user has selected a flight
-        // and then call api.setFlight(chosenFlight)
-        //if the user hasn't selected a flight return prevent him from proceeding
-        $location.path('/');
-    }
+  if ($routeParams.returnDate) {
+    var returnDate = new Date($routeParams.returnDate * 1000);
+    $scope.roundTrip = true;
+  }
 
-    $scope.selectedBooking = {
-      "PassengerDetails": null,
-      "refExitFlightID": null,
-      "refReEntryFlightID": null,
-      "class": null,
-      "paymentToken": null
-    };
-    var origin = $routeParams.origin;
-    var destination = $routeParams.destination;
-    var exitDate = new Date($routeParams.exitDate * 1000);
-    $scope.roundTrip = false;
-    if ($routeParams.returnDate) {
-        var returnDate = new Date($routeParams.returnDate * 1000);
-        $scope.roundTrip = true;
-    }
-    var flights;
-    var returnDateMill;
-    if (returnDate)
-        returnDateMill = returnDate.getTime();
-    console.log(exitDate.toISOString())
-    api.getOtherFlightsEco(origin, destination, exitDate.getTime(), returnDateMill).then(function mySuccess(response) {
-        console.log(response)
-        $scope.flights = response.data;
-    }, function myError(response) {
-        console.log(response.statusText);
-    })
+  var flights;
+  var returnDateMill;
+
+  if (returnDate)
+    returnDateMill = returnDate.getTime();
+
+  api.getOtherFlightsEco(origin, destination, exitDate.getTime(), returnDateMill).then(function mySuccess(response) {
+    $scope.flights = response.data;
+  }, function myError(response) {
+    console.log(response.statusText);
+  })
+
 }
 
 
-if(Type == 'mobile'){
+if (Type == 'mobile') {
   flightNewController.$inject = ['$scope', '$location', 'api'];
-}else{
-  flightNewController.$inject = ['$scope', '$location','$routeParams', 'api'];
+} else {
+  flightNewController.$inject = ['$scope', '$location', '$routeParams', 'api'];
 }
 
 
