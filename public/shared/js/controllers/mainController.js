342054e4c7d1411305dc4f4fbd97be72f448ee5d
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2016-05-02 01:41:17 +0200

    Fix scope issue in main ionic view

--- public/shared/js/controllers/mainController.js
+++ public/shared/js/controllers/mainController.js
@@ -1,125 +1,150 @@
 App.controller('mainCtrl', function($scope, $location, api) {
-    $scope.pageClass = 'page-main';
+  $scope.pageClass = 'page-main';
+
+  $scope.go = function() {
+    console.log($scope.selectedOrigin);
+  }
+
+  api.getAirports().then(function mySucces(response) {
+    $scope.airports = response.data;
+  }, function myError(response) {
+    console.log(response.statusText);
+  });
 
 
-      api.getAirports().then(function mySucces(response) {
-          $scope.airports = response.data;
-      }, function myError(response) {
-          console.log(response.statusText);
-      });
-      $scope.selectedOrigin = undefined;
-      $scope.selectedDest = undefined;
+  $scope.selectedOrigin = undefined;
+  $scope.selectedDest = undefined;
 
-      function airporsContains(iata) {
-          for (var i = 0; i < $scope.airports.length; i++) {
-              if (iata == $scope.airports[i]['iata'])
-                  return true;
-          }
-          return false;
-      }
-
-      $scope.buttonState = function() {
-          return !$scope.selectedOrigin || !$scope.selectedDest || !$scope.exitDate || $scope.selectedDest == $scope.selectedOrigin || !airporsContains($scope.selectedOrigin) || !airporsContains($scope.selectedDest);
-      }
-
-
-    if(Type == 'desktop'){
-
-      $('#main-text').typeIt({
-          strings: [
-              "Simple, convenient, instant confirmation.", "Destinations all around the globe.", "Experience authentic hospitality.", "Time to get enchanted."
-          ],
-          speed: 120,
-          breakLines: false,
-          loop: true
-      });
-      $scope.flight = {
-          type: "one"
-      }
-      $scope.otherAirline = {
-      value:false
-      }
-      $scope.goToFlights = function() {
-          if ($scope.otherAirline.value) {
-            if ($scope.flight.type == "one")
-            $location.path('/flights-new').search('origin', $scope.selectedOrigin).search('destination', $scope.selectedDest).search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0));
-            else{
-              $location.path('/flights-new')
-                  .search('origin', $scope.selectedOrigin)
-                  .search('destination', $scope.selectedDest)
-                  .search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0))
-                  .search('returnDate', ($scope.returnDate.getTime() / 1000).toFixed(0));
-            }
-          } else {
-              if ($scope.flight.type == "one")
-                  $location.path('/flights').search('origin', $scope.selectedOrigin).search('destination', $scope.selectedDest).search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0));
-              else {
-                  $location.path('/flights')
-                      .search('origin', $scope.selectedOrigin)
-                      .search('destination', $scope.selectedDest)
-                      .search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0))
-                      .search('returnDate', ($scope.returnDate.getTime() / 1000).toFixed(0));
-              }
-
-          }
-
-      };
-      $location.url($location.path());
-      setUpDate($scope);
-
-      $scope.children = ['0 children', '1 child', '2 children', '3 children', '4 children'];
-      $scope.childrenBtnText = $scope.children[0];
-      $scope.changeChildren = function(text) {
-          $scope.childrenBtnText = text;
-      }
-
-
-
-      $scope.adults = ['1 adult', '2 adults', '3 adults', '4 adults'];
-      $scope.adultBtnText = $scope.adults[0];
-      $scope.changeAdult = function(text) {
-          $scope.adultBtnText = text;
-      }
-
-      $scope.infants = ['0 infants', '1 infant'];
-      $scope.infantBtnText = $scope.infants[0];
-      $scope.changeInfant = function(text) {
-          $scope.infantBtnText = text;
-      }
-    }else{
+  function airporsContains(iata) {
+    for (var i = 0; i < $scope.airports.length; i++) {
+      if (iata == $scope.airports[i]['iata'])
+        return true;
     }
+    return false;
+  }
+
+  $scope.buttonState = function() {
+    return !$scope.selectedOrigin || !$scope.selectedDest || !$scope.exitDate|| $scope.selectedDest == $scope.selectedOrigin || !airporsContains($scope.selectedOrigin) || !airporsContains($scope.selectedDest);
+  }
+
+  $scope.flight = {
+    type: "one"
+  }
+  $scope.otherAirline = {
+    value: false
+  }
+
+
+
+  $scope.goToFlights = function() {
+    var exitDate, returnDate;
+    console.log($scope.selectedOrigin)
+      // if(Type == 'desktop'){
+    exitDate = ($scope.exitDate.getTime() / 1000).toFixed(0);
+    returnDate = ($scope.returnDate.getTime() / 1000).toFixed(0);
+    // }else{
+    //   exitDate = ((new Date($scope.exitDate).getTime())/1000).toFixed(0);
+    //   returnDate = ((new Date($scope.returnDate).getTime())/1000).toFixed(0);
+    // }
+    if ($scope.otherAirline.value) {
+      if ($scope.flight.type == "one")
+        $location.path('/flights-new')
+        .search('origin', $scope.selectedOrigin)
+        .search('destination', $scope.selectedDest)
+        .search('exitDate', exitDate);
+      else {
+        $location.path('/flights-new')
+          .search('origin', $scope.selectedOrigin)
+          .search('destination', $scope.selectedDest)
+          .search('exitDate', exitDate)
+          .search('returnDate', returnDate);
+      }
+    } else {
+      if ($scope.flight.type == "one")
+        $location.path('/flights').search('origin', $scope.selectedOrigin).search('destination', $scope.selectedDest).search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0));
+      else {
+        $location.path('/flights')
+          .search('origin', $scope.selectedOrigin)
+          .search('destination', $scope.selectedDest)
+          .search('exitDate', ($scope.exitDate.getTime() / 1000).toFixed(0))
+          .search('returnDate', ($scope.returnDate.getTime() / 1000).toFixed(0));
+      }
+
+    }
+
+  };
+
+
+
+
+  if (Type == 'desktop') {
+
+    $('#main-text').typeIt({
+      strings: [
+        "Simple, convenient, instant confirmation.", "Destinations all around the globe.", "Experience authentic hospitality.", "Time to get enchanted."
+      ],
+      speed: 120,
+      breakLines: false,
+      loop: true
+    });
+
+
+
+    $location.url($location.path());
+    setUpDate($scope);
+
+    $scope.children = ['0 children', '1 child', '2 children', '3 children', '4 children'];
+    $scope.childrenBtnText = $scope.children[0];
+    $scope.changeChildren = function(text) {
+      $scope.childrenBtnText = text;
+    }
+
+
+
+    $scope.adults = ['1 adult', '2 adults', '3 adults', '4 adults'];
+    $scope.adultBtnText = $scope.adults[0];
+    $scope.changeAdult = function(text) {
+      $scope.adultBtnText = text;
+    }
+
+    $scope.infants = ['0 infants', '1 infant'];
+    $scope.infantBtnText = $scope.infants[0];
+    $scope.changeInfant = function(text) {
+      $scope.infantBtnText = text;
+    }
+  }
 });
 
 function setUpDate($scope) {
-    $scope.today = function() {
-        $scope.exitDate = new Date();
-        $scope.returnDate = new Date();
-    };
-    $scope.today();
+  $scope.today = function() {
+    $scope.exitDate = new Date();
+    $scope.returnDate = new Date();
+  };
+  $scope.today();
 
-    $scope.open2 = function() {
-        $scope.popup2.opened = true;
-    };
-    $scope.open = function() {
-        $scope.popup.opened = true;
-    };
+  $scope.open2 = function() {
+    $scope.popup2.opened = true;
+  };
+  $scope.open = function() {
+    $scope.popup.opened = true;
+  };
 
 
-    function disabled(data) {
-        var date = data.date,
-            mode = data.mode;
-        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-    }
-    $scope.dateOptions = {
-        formatYear: 'yy',
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-    };
-    $scope.popup2 = {
-        opened: false
-    };
-    $scope.popup = {
-        opened: false
-    };
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+  $scope.popup2 = {
+    opened: false
+  };
+  $scope.popup = {
+    opened: false
+  };
 }
